1f937f8721104b22e66b81a168ab9981a94f849f
roydonders <timonotter@gmail.com>
2024-02-19 16:45:56 +0100

    aparte data reading

---
+++ data_reader.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+class DataReader:
+    def __init__(self, file_path):
+        self.lab_path = file_path
+
+    def read_data(self):
+        #Method for reading data
+        import_roads = pd.read_table(self.lab_path + '\infrastructure\_roads.tsv', low_memory=False)
+
+        # Make a copy of the imported dataframe to bypass importing each time you want your original dataframe (due to a mistake for example)
+        df_roads = import_roads
+
+        columns = df_roads.columns
+
+        # Define the new column names pattern
+        new_column_names = {}
+        for i, col in enumerate(columns[1:], start=1):
+            if i % 3 == 1:
+                new_column_names[col] = 'LRP'
+            elif i % 3 == 2:
+                new_column_names[col] = 'Lat'
+            else:
+                new_column_names[col] = 'Lon'
+
+        # Rename the columns using the dictionary
+        df_roads.rename(columns=new_column_names, inplace=True)
+
+        # reshape the DataFrame into a tidy dataset
+        df_roads_tidy = pd.DataFrame({
+            'Road': df_roads['road'].repeat(len(df_roads.columns) // 3),
+            'LRP': df_roads['LRP'].values.reshape(-1),
+            'Lat': df_roads['Lat'].values.reshape(-1),
+            'Lon': df_roads['Lon'].values.reshape(-1)})
+
+        # This line can be enabeled to drop missing values, see the section on 'Missing value issue' to see for which roads this would be usefull
+        # df_roads_tidy = df_roads_tidy.dropna()
+
+        df_roads_tidy = df_roads_tidy.reset_index(drop=True)
+
+        print("hello world")
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from data_reader import DataReader
+
 def main():
     #Filepath Lukas
     #lab_path = r'C:\Users\lukas\Downloads\Asim Lab1\WBSIM_Lab1_2024\WBSIM_Lab1_2024'
@@ -14,41 +16,7 @@ def main():
     lab_path = r'C:\Users\TOT\OneDrive - Stichting EBO de Passie\Documenten\Studie\Advanced Simulation\WBSIM_Lab1_2024\WBSIM_Lab1_2024'
 
     #Import roads.tsv dataframe
-    import_roads = pd.read_table(lab_path + '\infrastructure\_roads.tsv', low_memory=False)
-
-    #Make a copy of the imported dataframe to bypass importing each time you want your original dataframe (due to a mistake for example)
-    df_roads = import_roads
-
-    columns = df_roads.columns
-
-    # Define the new column names pattern
-    new_column_names = {}
-    for i, col in enumerate(columns[1:], start=1):
-        if i % 3 == 1:
-            new_column_names[col] = 'LRP'
-        elif i % 3 == 2:
-            new_column_names[col] = 'Lat'
-        else:
-            new_column_names[col] = 'Lon'
-
-    # Rename the columns using the dictionary
-    df_roads.rename(columns=new_column_names, inplace=True)
-
-
-
-    # reshape the DataFrame into a tidy dataset
-    df_roads_tidy = pd.DataFrame({
-        'Road': df_roads['road'].repeat(len(df_roads.columns) // 3),
-        'LRP': df_roads['LRP'].values.reshape(-1),
-        'Lat': df_roads['Lat'].values.reshape(-1),
-        'Lon': df_roads['Lon'].values.reshape(-1) })
-
-    # This line can be enabeled to drop missing values, see the section on 'Missing value issue' to see for which roads this would be usefull
-    #df_roads_tidy = df_roads_tidy.dropna()
-
-    df_roads_tidy = df_roads_tidy.reset_index(drop=True)
-
-    print("hello world")
+    reader = DataReader(lab_path)
 
     # This function takes an integer as input and finds the n-th road in the datafile
     # it returns the subsetted dataframe related to that road

884550d080d9c4452963481f526c23317668710f
roydonders <timonotter@gmail.com>
2024-02-20 17:44:25 +0100

    problem identifier add

--- problem_identifier.py
+++ problem_identifier.py
@@ -12,12 +12,17 @@ class OutOfRangeProblem(DataQualityProblem):
     def __init__(self, inf):
         super().__init__(inf, "Out of range")
 
+class LatLongSwitchedProblem(DataQualityProblem):
+    def __init__(self, inf):
+        super().__init__(inf, "Lat/Long switched")
+
 class IncorrectDecimalProblem(DataQualityProblem):
     def __init__(self, inf):
         super().__init__(inf, "Incorrect decimal")
 
 class ProblemIdentifier:
-    def __init__(self, file_path):
+    def __init__(self):
         # checks for problems
+        # If lat/long not in bangladesh - check if this or this is the case
         pass
 

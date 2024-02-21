bd047db33dea97f7cf396dbd09de79de77087644
roydonders <timonotter@gmail.com>
2024-02-21 13:53:13 +0100

    iets meer over data exporting 3

--- data_exporter.py
+++ data_exporter.py
@@ -1,17 +1,21 @@
 # moet het weer naar de oorspronkelijke files krijgen!!
-
+import pandas as pd
+from pandas import DataFrame
 
 class DataExporter:
     def __init__(self, output_path, finalbridges):
         self.output_path = output_path
         self.finalbridges = finalbridges
+        self.dfbridges = DataFrame()
         self.prepareExport()
 
     def prepareExport(self):
         self.prepareExportSimple()
 
     def prepareExportSimple(self):
-        pass
+        # per bridge in the final bridge list, just append all the df rows per object.
+        for bridge in self.finalbridges:
+            self.dfbridges.append(bridge.df)
 
     def export(self):
         pass

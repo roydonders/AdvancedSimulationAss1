b2c8c30d31f423c4d7ac18e3f35390d9b4a0f277
Fred <f6185442514@live.nl>
2024-02-23 21:55:01 +0100

    documentation in road analyses, creating roadcleaner class with static methods for analysing roads

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import openpyxl as opl
-
 from data_reader import DataReader
 from data_exporter import DataExporter
 from problem_identifier import ProblemIdentifier
+
 
 def main():
     # Main program method providing an overview
     print("Start Data Cleaning!")
 
-    #Import all data
+    # Import all data
     reader = DataReader()
     reader.read_data()
 
-    #Collect results of read data:
+    # Collect results of read data:
     # In DataFrames (df bridges not used currently)
     df_roads_tidy = reader.df_roads_tidy
     df_bridges_tidy = reader.df_bridges_tidy
@@ -25,14 +21,14 @@ def main():
     bridges = reader.bridges_list
     print("Bridges are read from bridges_list")
 
-    #Identify and Solve data problems
+    # Identify and Solve data problems
     print("Identifying Problems in the Data for roads and bridges")
-    pi = ProblemIdentifier(roads,bridges)
+    pi = ProblemIdentifier(roads, bridges)
     print("Cleaning the Data using Heuristics")
     finalroads, finalbridges = pi.solve()
     print("Road and bridge issues solved")
 
-    #Output the changed files
+    # Output the changed files
     output_path = reader.data_path
     exporter = DataExporter(output_path, finalbridges)
     exporter.export()
--- roads_analyses.py
+++ roads_analyses.py
@@ -3,11 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import statistics as stat
-%matplotlib inline
 
 # import pd.read_table data/_roads.tsv"
-    # data reader timon
-        #levert al tidy data
+# data reader timon
+# levert al tidy data
 
 # make a df of the roads.tsv
 
@@ -16,96 +15,151 @@ import statistics as stat
 
 # functies voor schoonmaken van roads data
 
-def load_and_clean(file):
-    road_length = []
-    for i in file.index[:-1]:
-        road_length.append(
-            math.sqrt((file['Lat'][i] - file['Lat'][i + 1]) ** 2 + (file['Lon'][i] - file['Lon'][i + 1]) ** 2))
 
-    while road_length and road_length[-1] != road_length[-1]:
-        road_length.pop()
+class RoadCleaner:
+    def __init__(self):
+        pass
 
-    road_length = [1 if math.isnan(x) else x for x in road_length]
+    @staticmethod
+    def find_road_df(n, df_roads_tidy):
+        """This function takes an integer as input and finds the n-th road in the datafile
+            it returns the subsetted dataframe related to that road"""
 
-    return road_length
+        road_specified = df_roads_tidy['Road'].unique()[n]
 
+        road_specified_df = df_roads_tidy.loc[df_roads_tidy['Road'] == road_specified]
 
-def check_boundaries(road_list, file, index):
-    if road_list[0] > (mean * TH):
-        if road_list[1] < (mean * TH):
-            print('First one of road', file['Road'][index], 'is possibly badly placed')
-            return True
+        return road_specified_df
 
-    if road_list[-1] > (mean * TH):
-        if road_list[-2] < (mean * TH):
-            print('Last one of road', file['Road'][index], 'is possibly badly placed')
-            return True
+    @staticmethod
+    def correct_road(self, index, file):
+        # Make a copy of the DataFrame to avoid modifying the original data
+        corrected_file = self.file.copy()
 
-    else:
-        return False
+        # Calculate the average values for 'Lat' and 'Lon' and assign them to the corresponding row
+        corrected_file.loc[index + 1, 'Lat'] = (file.loc[index, 'Lat'] + file.loc[index + 2, 'Lat']) / 2
+        corrected_file.loc[index + 1, 'Lon'] = (file.loc[index, 'Lon'] + file.loc[index + 2, 'Lon']) / 2
 
-def clean_sheet(file):
-    cleaned_file = file.copy()
+        return corrected_file
 
-    for i in range(file.index[0], a):
-        if np.isnan(file['Lat'][i]) or np.isnan(file['Lon'][i]):
-            if not np.isnan(file['Lat'][i]) or np.isnan(file['Lon'][i]):
-                file = single_NaN_cleaner(file.index[0] + i, file)
-    return
+    @staticmethod
+    def load_and_clean(file):
+        road_length = []
+        for i in file.index[:-1]:
+            road_length.append(
+                math.sqrt((file['Lat'][i] - file['Lat'][i + 1]) ** 2 + (file['Lon'][i] - file['Lon'][i + 1]) ** 2))
 
-def correct_road(index, file):
-    # Make a copy of the DataFrame to avoid modifying the original data
-    corrected_file = file.copy()
+        while road_length and road_length[-1] != road_length[-1]:
+            road_length.pop()
 
-    # Calculate the average values for 'Lat' and 'Lon' and assign them to the corresponding row
-    corrected_file.loc[index + 1, 'Lat'] = (file.loc[index, 'Lat'] + file.loc[index + 2, 'Lat']) / 2
-    corrected_file.loc[index + 1, 'Lon'] = (file.loc[index, 'Lon'] + file.loc[index + 2, 'Lon']) / 2
+        road_length = [1 if math.isnan(x) else x for x in road_length]
 
-    return corrected_file
+        return road_length
 
-def check_roads(road_list,file):
-    size_list = len(road_list)
-    for i in range(size_list):
-        if road_list[i]>mean*TH or road_list[i]>0.01:
-            if road_list[i+1]>(mean*TH) or road_list[i+1]>0.01:
-                index_road = file.index[i]
-                file = correct_road(index_road,file)
-    return file
+    @staticmethod
+    def check_boundaries(road_list, file, index):
+        # calculate  the mean of the roads
+        mean = stat.mean(road_list)
+        # set a threshold of 10
+        TH = 10  # Threshold
 
-def correct_multiple(index, file, wrongs):
-    corrected_file = file.copy()
+        if road_list[0] > (mean * TH):
+            if road_list[1] < (mean * TH):
+                print('First one of road', file['Road'][index], 'is possibly badly placed')
+                return True
 
-    for i in range(wrongs):
-        m = i + 1
-        checker = index + m
+        if road_list[-1] > (mean * TH):
+            if road_list[-2] < (mean * TH):
+                print('Last one of road', file['Road'][index], 'is possibly badly placed')
+                return True
 
-        # Use .loc to set values in the DataFrame
-        corrected_file.loc[checker, 'Lat'] = file['Lat'][index] + m * (
-                    file['Lat'][index + wrongs + 1] - file['Lat'][index]) / (wrongs + 1)
-        corrected_file.loc[checker, 'Lon'] = file['Lon'][index] + m * (
-                    file['Lon'][index + wrongs + 1] - file['Lon'][index]) / (wrongs + 1)
+        else:
+            return False
 
-    return corrected_file
+    # zat in de code van bram maar wordt volgens mij niet gebruikt
+    # def clean_sheet(file):
+    #     cleaned_file = file.copy()
+    #
+    #     for i in range(file.index[0], a):
+    #         if np.isnan(file['Lat'][i]) or np.isnan(file['Lon'][i]):
+    #             if not np.isnan(file['Lat'][i]) or np.isnan(file['Lon'][i]):
+    #                 file = single_NaN_cleaner(file.index[0] + i, file)
+    #     return
 
+    def check_roads(self, road_list, file):
+        # calculate  the mean of the roads
+        mean = stat.mean(road_list)
+        # set a threshold of 10
+        TH = 10  # Threshold
+        # these need to be made global
 
-def check_multiple(road_list, file):
-    size_list = len(road_list)
-    corrected_file = file.copy()
-    check_list = road_list.copy()
-    for i in range(size_list):  # Adjust range to avoid index out of bounds
-        if check_list[i] > mean * TH:
-            if check_list[i + 1] < (mean * TH):
-                z = 1
-                while check_list[i + z] < (mean * TH) and i + z < size_list:
-                    z = z + 1
-                index_road = file.index[i]
-                corrected_file = correct_multiple(index_road, corrected_file, z)
-                check_list = load_and_clean(corrected_file)
-    return corrected_file
+        size_list = len(road_list)
+        for i in range(size_list):
+            if road_list[i] > mean * TH or road_list[i] > 0.01:
+                if road_list[i+1] > (mean * TH) or road_list[i+1] > 0.01:
+                    index_road = file.index[i]
+                    file = self.correct_road(index_road, file)
+        return file
+
+    @staticmethod
+    def correct_multiple(index, file, wrongs):
+        corrected_file = file.copy()
+
+        for i in range(wrongs):
+            m = i + 1
+            checker = index + m
+
+            # Use .loc to set values in the DataFrame
+            corrected_file.loc[checker, 'Lat'] = file['Lat'][index] + m * (
+                        file['Lat'][index + wrongs + 1] - file['Lat'][index]) / (wrongs + 1)
+            corrected_file.loc[checker, 'Lon'] = file['Lon'][index] + m * (
+                        file['Lon'][index + wrongs + 1] - file['Lon'][index]) / (wrongs + 1)
+
+        return corrected_file
+
+    def check_multiple(self, road_list, file):
+        # calculate  the mean of the roads
+        mean = stat.mean(road_list)
+        # set a threshold of 10
+        TH = 10  # Threshold
+        # these need to be made global
+
+        size_list = len(road_list)
+        corrected_file = file.copy()
+        check_list = road_list.copy()
+        for i in range(size_list):  # Adjust range to avoid index out of bounds
+            if check_list[i] > mean * TH:
+                if check_list[i + 1] < (mean * TH):
+                    z = 1
+                    while check_list[i + z] < (mean * TH) and i + z < size_list:
+                        z = z + 1
+                    index_road = file.index[i]
+                    corrected_file = self.correct_multiple(index_road, corrected_file, z)
+                    check_list = self.load_and_clean(corrected_file)
+        return corrected_file
+
+    # deze nog fixen van lukas om naar tsv te exporteren
+    @staticmethod
+    def df_to_tsv(df_cleaned):
+        # Create a multi-index with Road and Index
+        df_cleaned.set_index(['Road', df_cleaned.groupby(['Road']).cumcount() + 1], inplace=True)
+
+        # Reshape the dataframe
+        df_output = df_cleaned.unstack().sort_index(axis=1, level=1)
+
+        # Flatten the columns
+        df_output.columns = ['_'.join(map(str, i)) for i in df_output.columns]
+        df_output.reset_index(inplace=True)
+
+        # Write cleaned DataFrame to TSV file called '_roads_cleaned.tsv'
+        file_path = r'AdvancedSimulationAss1\roads_cleaned.tsv'
+        df_output.to_csv(file_path, sep='\t', index=False)
+        return
+
 
 # correct_road en correct multiple geven de hele roads df's
 
-# Methode van Lukas op whatssapp gebruiken om df om te zetten naar df
+# Methode van Lukas op whatsapp gebruiken om df om te zetten naar df
 
 #
 # # Eerste cell commands
